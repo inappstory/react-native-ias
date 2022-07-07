993963cf299d985f19af2394881affaa56e2e224
artemstrebizh <artem.strebizh@kiozk.ru>
2022-07-07 16:35:47 +0300

    chore(🚀): 0.2.1
    fix showStory types
    fix showOnboarding types

--- types/StoryManager.d.ts
+++ types/StoryManager.d.ts
@@ -1,5 +1,6 @@
 import {Option} from "./types";
 import EventEmitter from "./EventEmitter";
+import AppearanceManager from "./AppearanceManager";
 
 export declare type StoryManagerConfig = {
   apiKey: string;
@@ -9,8 +10,22 @@ export declare type StoryManagerConfig = {
   lang?: string;
 };
 
+export declare type OnboardingLoadStatus = {
+    feed: string|number,
+    defaultListLength: number,
+    favoriteListLength: number,
+    success: boolean,
+    error: Option<{
+        name: string,
+        networkStatus: number,
+        networkMessage: string
+    }>
+};
+
 declare class StoryManager extends EventEmitter {
   constructor(config: StoryManagerConfig);
+  showStory(storyId: number|string, appearanceManager: AppearanceManager): Promise<{loaded: boolean}>;
+  showOnboardingStories(appearanceManager: AppearanceManager, customTags?: Array<string>): Promise<OnboardingLoadStatus>;
 }
 
 export default StoryManager;

0f1728c0a73ea97bdf6c369a7829c1cd23216d31
a.strebizh <artem.strebizh@inappstory.com>
2023-04-06 12:49:41 +0300

    build(🚀): 0.2.27
    fix: favorite button icon svg src
    fix: make AppearanceManager options Partial
    fix: [Android] WebView - prevent forceDarkMode (Android algorithmic Darkening)

--- types/AppearanceManager.d.ts
+++ types/AppearanceManager.d.ts
@@ -1,51 +1,51 @@
-import {Option} from "./types";
+import { Option } from "./types";
 
-export declare type AppearanceCommonOptions = {
-  hasFavorite?: boolean;
-  hasLike?: boolean;
-  hasShare?: boolean;
-};
+export declare type AppearanceCommonOptions = Partial<{
+  hasFavorite: boolean;
+  hasLike: boolean;
+  hasShare: boolean;
+}>;
 
-export declare type StoriesListOptions = {
+export declare type StoriesListOptions = Partial<{
   title: StoriesListTitleOptions;
   card: StoriesListCardOptions;
   favoriteCard: StoriesListFavoriteCardOptions;
-  layout: {
+  layout: Partial<{
     height: number;
     backgroundColor: string;
-      sliderAlign: StoriesListSliderAlign;
-  };
+    sliderAlign: StoriesListSliderAlign;
+  }>;
   sidePadding: number;
   topPadding: number;
   bottomPadding: number;
   bottomMargin: number;
 
-  navigation: {
+  navigation: Partial<{
     showControls: boolean;
     controlsSize: number;
     controlsBackgroundColor: string;
     controlsColor: string;
-  };
+  }>;
 
-  extraCss?: string;
+  extraCss: string;
 
   // setCallback ?
-  handleStoryLinkClick?: (payload: StoriesListClickEvent) => void;
+  handleStoryLinkClick: (payload: StoriesListClickEvent) => void;
 
-  handleStartLoad?: (loaderContainer: HTMLElement) => void;
-  handleStopLoad?: (loaderContainer: HTMLElement) => void;
+  handleStartLoad: (loaderContainer: HTMLElement) => void;
+  handleStopLoad: (loaderContainer: HTMLElement) => void;
 
 
   // handleClickOnStory?: (event: StoriesListClickEvent) => void,
 
-}
+}>;
 
-export declare type StoriesListTitleOptions = {
+export declare type StoriesListTitleOptions = Partial<{
   content: string;
   color: string;
   font: string;
   marginBottom: number;
-}
+}>;
 
 export declare enum StoriesListCardViewVariant {
   CIRCLE = 'circle',
@@ -71,8 +71,8 @@ export declare enum StoriesListCardTitlePosition {
     CARD_OUTSIDE_BOTTOM = 'cardOutsideBottom'
 }
 
-export declare type StoriesListCardOptions = {
-  title: {
+export declare type StoriesListCardOptions = Partial<{
+  title: Partial<{
     color: string;
     padding: string | number;
     font: string;
@@ -80,60 +80,60 @@ export declare type StoriesListCardOptions = {
       textAlign: StoriesListCardTitleTextAlign;
       position: StoriesListCardTitlePosition;
       lineClamp: number;
-  };
+  }>;
   gap: number;
   height: number;
   variant: StoriesListCardViewVariant;
-  border: {
+  border: Partial<{
     radius: number;
     color: string;
     width: number;
     gap: number;
-  };
+  }>;
   boxShadow: Option<string>;
-    dropShadow?: Option<string>;
+    dropShadow: Option<string>;
   opacity: Option<number>;
-  mask: {
+  mask: Partial<{
     color: Option<string>;
-  };
-    svgMask?: Option<{
+  }>;
+    svgMask: Partial<Option<{
         cardMask: Option<string>;
         overlayMask: Array<{
             mask: Option<string>;
             background: Option<string>;
         }>;
-    }>;
-  opened: {
-    border: {
+    }>>;
+  opened: Partial<{
+    border: Partial<{
       radius: Option<number>;
       color: Option<string>;
       width: Option<number>;
       gap: Option<number>;
-    };
+    }>;
     boxShadow: Option<string>;
-      dropShadow?: Option<string>;
+      dropShadow: Option<string>;
     opacity: Option<number>;
-    mask: {
+    mask: Partial<{
       color: Option<string>;
-    };
-      svgMask?: Option<{
+    }>;
+      svgMask: Partial<Option<{
           cardMask: Option<string>;
           overlayMask: Array<{
               mask: Option<string>;
               background: Option<string>;
           }>;
-      }>;
-  };
-}
+      }>>;
+  }>;
+}>;
 
-export declare type StoriesListFavoriteCardOptions = StoriesListCardOptions & {
-  title: {
+export declare type StoriesListFavoriteCardOptions = StoriesListCardOptions & Partial<{
+  title: Partial<{
     content: string;
     color: string;
     padding: string | number;
     font: string;
-  }
-};
+  }>
+}>;
 
 export declare type StoriesListClickEvent = {
   id: number,
@@ -147,13 +147,13 @@ export declare enum StoriesListEvents {
   END_LOADER = 'endLoad',
 }
 
-export declare type StoryFavoriteReaderOptions = {
-  title?: {
+export declare type StoryFavoriteReaderOptions = Partial<{
+  title: Partial<{
     content: string;
       font: string;
       color: string;
-  }
-};
+  }>
+}>;
 
 export declare enum StoryReaderCloseButtonPosition {
   LEFT = 'left',
@@ -167,18 +167,18 @@ export declare enum StoryReaderSwipeStyle {
 }
 
 
-export declare type StoryReaderOptions = {
+export declare type StoryReaderOptions = Partial<{
   closeButtonPosition: StoryReaderCloseButtonPosition,
   scrollStyle: StoryReaderSwipeStyle,
-  loader?: {
-    default: {
+  loader: Partial<{
+    default: Partial<{
       color: Option<string>,
       accentColor: Option<string>,
-    },
+    }>,
     custom: Option<string>
-  },
-  recycleStoriesList?: boolean,
-  closeOnLastSlideByTimer?: boolean,
+  }>,
+  recycleStoriesList: boolean,
+  closeOnLastSlideByTimer: boolean,
     closeButton: {
         svgSrc: {
             baseState: string
@@ -213,7 +213,7 @@ export declare type StoryReaderOptions = {
             baseState: string,
         }
     }
-};
+}>;
 
 declare class AppearanceManager {
   public setCommonOptions(options: AppearanceCommonOptions): AppearanceManager;
